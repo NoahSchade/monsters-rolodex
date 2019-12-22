cc0f7ea23541bd43dc131afe28d0259977e6e447
NoahSchade <noah9@mail.com>
2019-12-22 00:54:17 -0500

    Updates

---
+++ precache-manifest.a8d51cf95b88b8b80dd78f28bc95cc06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88dc8d5e09684e7bf470f13efd5c76e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "845ba3a4a263d2d08848",
+    "url": "/monsters-rolodex/static/css/main.84ae8cca.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "845ba3a4a263d2d08848",
+    "url": "/monsters-rolodex/static/js/main.e7e40c61.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
